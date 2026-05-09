ac114fec1bb75fb8a020d1030761190fa8b87635
furkankusbeygi <furkanksb3509@gmail.com>
2026-05-09 04:20:41 +0300

    vercel hazirlik

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Bu satır sitenin HTML olarak çıkmasını sağlar
-  images: {
-    unoptimized: true, // Statik sitede resim optimizasyonu için şart
-  },
-};
-
-export default nextConfig;
